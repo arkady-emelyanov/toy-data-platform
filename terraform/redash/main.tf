d3b65c8ccee04865eebf2ea64bd4d9d133c6fedb
Arkady Emelyanov <arkady.emelyanov@gmail.com>
2021-02-26 23:31:01 +0300

    redash scheduler and worker

--- terraform/redash/main.tf
+++ terraform/redash/main.tf
@@ -4,6 +4,18 @@ locals {
     app = local.module_name
   }
 
+  server_labels = merge(local.module_labels, {
+    component = "server"
+  })
+
+  scheduler_labels = merge(local.module_labels, {
+    component = "scheduler"
+  })
+
+  worker_labels = merge(local.module_labels, {
+    component = "worker"
+  })
+
   client_port = 5000
 
   redis_url = "redis://${var.redis_endpoint}/${var.redis_database}"
@@ -21,6 +33,7 @@ resource "kubernetes_config_map" "config" {
     REDASH_REDIS_URL = local.redis_url
     REDASH_DATABASE_URL = local.postgres_url
     REDASH_WEB_WORKERS = "1"
+    WORKERS_COUNT = "1"
   }
 }
 
@@ -31,7 +44,7 @@ resource "kubernetes_service" "service" {
     labels = local.module_labels
   }
   spec {
-    selector = local.module_labels
+    selector = local.server_labels
     port {
       port = local.client_port
       target_port = local.client_port
@@ -44,16 +57,16 @@ resource "kubernetes_deployment" "server" {
   metadata {
     name = local.module_name
     namespace = var.namespace
-    labels = local.module_labels
+    labels = local.server_labels
   }
   spec {
     replicas = 1
     selector {
-      match_labels = local.module_labels
+      match_labels = local.server_labels
     }
     template {
       metadata {
-        labels = local.module_labels
+        labels = local.server_labels
       }
       spec {
         init_container {
@@ -97,6 +110,72 @@ resource "kubernetes_deployment" "server" {
             http_get {
               path = "/ping"
               port = "client"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "scheduler" {
+  metadata {
+    name = "${local.module_name}-scheduler"
+    namespace = var.namespace
+    labels = local.scheduler_labels
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = local.scheduler_labels
+    }
+    template {
+      metadata {
+        labels = local.scheduler_labels
+      }
+      spec {
+        container {
+          name = "scheduler"
+          image_pull_policy = "IfNotPresent"
+          image = var.server_image
+          args = ["scheduler"]
+
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.config.metadata[0].name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "worker" {
+  metadata {
+    name = "${local.module_name}-worker"
+    namespace = var.namespace
+    labels = local.worker_labels
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = local.worker_labels
+    }
+    template {
+      metadata {
+        labels = local.worker_labels
+      }
+      spec {
+        container {
+          name = "worker"
+          image_pull_policy = "IfNotPresent"
+          image = var.server_image
+          args = ["worker"]
+
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.config.metadata[0].name
             }
           }
         }
